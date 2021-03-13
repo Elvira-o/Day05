cb17c338087272527f895aa204d57772f22609c7
Elvira-o <elvira2021ot@gmail.com>
2021-03-13 02:31:58 +0100

    Word Blanks

---
+++ ex14/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+  var myNoun = "dog";
+  var myVerb = "ran";
+  var myAdjective = "big";
+  var myAdverb = "quickly";
+  var wordBlanks = "My " + myNoun + " " + myVerb + " " + myAdjective + " " + myAdverb + ".";
+  return wordBlanks;
+}
+console.log(main());
+module.exports = main;
